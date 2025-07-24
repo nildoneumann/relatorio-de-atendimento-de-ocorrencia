0e4afafe05b6d227a1f12345646e97540bcef132
nildoneumann <leonildoneumann@gmail.com>
2025-07-24 07:28:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', () => {});
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request).catch(() => caches.match("offline.html")));
+});
